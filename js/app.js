759798bc24006af1ee84579588670141abe1e9e9
lenakosyakova <kosyakova@gmail.com>
2018-09-01 14:52:31 -0400

    Add collision, positions for player and enemies

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+let Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
@@ -22,7 +22,17 @@ Enemy.prototype.update = function(dt) {
     // bring enemy back to canvas && change speed
     if (this.x > 600) {
       this.x = -100;
-      this.speed = 100 + Math.floor(Math.random() * 200);
+      this.speed = 100 + Math.floor(Math.random() * 300);
+    }
+
+    // check for collision with player
+    if (this.x > player.x - 50 &&
+        this.x < player.x + 50 &&
+        this.y > player.y - 50 &&
+        this.y < player.y + 50) {
+      // return player to start
+      player.x = 202;
+      player.y = 400;
     }
 
 };
@@ -35,9 +45,7 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+let Player = function(x, y, speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
@@ -45,40 +53,68 @@ var Player = function(x, y, speed) {
 };
 
 Player.prototype.update = function(dt) {
+  // keep player within canvas
+    if (this.y > 380) {
+        this.y = 380;
+    }
 
+    if (this.x > 400) {
+        this.x = 400;
+    }
+
+    if (this.x < 0) {
+        this.x = 0;
+    }
+
+    if (this.y < 0) {
+      this.x = 202;
+      this.y = 400;
+    }
 };
 
 Player.prototype.render = function(){
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-Player.prototype.handleInput = function(allowedKey){
-  if (allowedKey === 'left' && this.x > 0){
+Player.prototype.handleInput = function(keyPressed){
+  if (keyPressed === 'left' && this.x > 0){
     this.x = this.x - 101;
   }
-  else if (allowedKey === 'up' && this.y > 0) {
+
+  if (keyPressed === 'up' && this.y > 0) {
     this.y = this.y - 83;
   }
-  else if (allowedKey === 'right' && this.x < 404) {
+
+  if (keyPressed === 'right' && this.x < 404) {
     this.x = this.x + 101;
   }
-  else if (allowedKey === 'down' && this.y < 374) {
+
+  if (keyPressed === 'down' && this.y < 374) {
     this.y = this.y + 83;
   }
+
 }
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
-var player = new Player();
+let allEnemies = [];
+let player = new Player(202, 400, 50);
 
+// Setup swimlanes to place enemies there
+let enemyPlace = [60, 145, 225];
+let enemy;
+
+enemyPlace.forEach(function(y) {
+    enemy = new Enemy(0, y, 100 + Math.floor(Math.random() * 300));
+    allEnemies.push(enemy);
+});
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
